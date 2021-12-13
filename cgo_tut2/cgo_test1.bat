52ed0c7e37935ed556c7129dace59c2c1611ef67
xiuxi <xiuxi554325746@gmail.com>
2021-12-14 01:28:25 +0800

    a

---
+++ cgo_tut2/cgo_test1.bat
@@ -0,0 +1,7 @@
+rem go clean
+rem touch main.go
+rem go run main.go
+rem go build main.go
+cls
+go build -a main.go
+main.exe
---
+++ cgo_tut2/hello.c
@@ -0,0 +1,6 @@
+//hello.c
+#include <stdio.h>
+
+void Hello(){
+    printf("Hello world xiuxi __________________ 8\n");
+}
